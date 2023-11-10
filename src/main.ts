5af1d36da113541eb29714903e5dd1f642cfe76e
呼海斌 <136257981@qq.com>
2023-11-10 20:11:05 +0800

    路由放到单独文件中

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,28 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from "vue-router"
+// import { createRouter, createWebHashHistory } from "vue-router"
 import './style.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia'
 import App from '@/App.vue'
+import router from '@/router/index'
 
-// 2. 定义一些路由
-// 每个路由都需要映射到一个组件。
-// 我们后面再讨论嵌套路由。
-const routes = [
-  { path: '/', component: () => import('@/views/home.vue') },
-  { path: '/about', component: () => import('@/views/about.vue') },
-]
+// // 2. 定义一些路由
+// // 每个路由都需要映射到一个组件。
+// // 我们后面再讨论嵌套路由。
+// const routes = [
+//   { path: '/', component: () => import('@/views/home.vue') },
+//   { path: '/about', component: () => import('@/views/about.vue') },
+// ]
 
-// 3. 创建路由实例并传递 `routes` 配置
-// 你可以在这里输入更多的配置，但我们在这里
-// 暂时保持简单
-const router = createRouter({
-  // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-  history: createWebHashHistory(),
-  routes, // `routes: routes` 的缩写
-})
+// // 3. 创建路由实例并传递 `routes` 配置
+// // 你可以在这里输入更多的配置，但我们在这里
+// // 暂时保持简单
+// const router = createRouter({
+//   // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+//   history: createWebHashHistory(),
+//   routes, // `routes: routes` 的缩写
+// })
 
 // 5. 创建并挂载根实例
 const app = createApp(App)
